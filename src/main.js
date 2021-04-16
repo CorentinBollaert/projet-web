cefc1f9388c1c32808245a722d671b3d080cb510
Corentin BOLLAERT-LUCIANI <cor_bo@hotmail.fr>
2021-04-16 16:59:01 +0200

    Ca marche

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css'
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 
+Vue.use(VueMaterial)
 Vue.config.productionTip = false;
 
 new Vue({
